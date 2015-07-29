62f20171054166d8a424da47c721791965eaa1c4
loyolu <loyolu@users.noreply.github.com>
2015-07-29 15:53:30 -0500

    saving to local storage, but still duplicating list

--- function.js
+++ function.js
@@ -1,60 +1,67 @@
 var list = [];
 
-//saves text box entries as var
-function recordText () {
-    $("#addbox").keyup(function (event) {
-        if (event.keyCode == 13) {
-        console.log("enter keyup\n");
-        var txt = $("#addbox").val();
-        console.log(txt);
-        $('#table-div').show;
-        displayNewItem(createData(txt));
-        $('#addbox').val('');
-   }});
-    $(document).on("click", "#addbutton", function(e) {
-        console.log("addbutton clicked\n");
-        var txt = $("#addbox").val();
-        console.log(txt);
-        $('#table-div').show;
-        displayNewItem(createData(txt));
-        $('#addbox').val('');
-    });
-    
+$(document).ready(function() {
+    $("#addbox").val("").focus();
+    loadData();
+    recordEvent();
+});
+
+function saveData () {
+    // save data to local storage
+    localStorage.setItem("notes",JSON.stringify(list));
 }
 
-function createId () {
-    console.log("creating id");
+function loadData () {
+    // load data from local storage
+    list = JSON.parse(localStorage.getItem("notes"));
+    if (list === null)
+     list = []
+}
+
+function recordEvent () {
+    $("#addbox").keyup(function (event) {
+        if (event.keyCode == 13)
+            recordEventHelper ();
+   });
+    $(document).on("click", "#addbutton", function(e) {
+        recordEventHelper ();
+    });
+}
+
+function recordEventHelper () {
+    console.log("event recorded on page: enter keyup or addbutton clicked\n");
+    var txt = $("#addbox").val();
+    console.log(txt);
+    createItem(txt);
+    displayItems(list);
+    $('#addbox').val('');        
+    console.log("Item created\n");    
+}
+
+function generateId() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
     });
 }
 
-function createData (text) {
+function createItem (text) {
     console.log("creating data");
     var newItem = {
         "description": text,
-        "id": createId()
+        "id": generateId()
     }
     console.log("appending new item\n");
     list.push(newItem);
     console.log(list, "\n");
-    return newItem;
+    saveData ();
+    return newItem.id;
 }
 
-
-$(document).ready(function() {
-    $("#addbox").val("").focus();
-    recordText();
-    if (list.length === 0) {
-        $('#table-div').hide;
-        console.log("Table hidden");
-    }
-});
-
-
-function displayNewItem(newItem) {
-    var idx = list.length;
-    var itemElement = $("<li class='notable table-view-cell' id=item'" +"-"+ idx + "'>" + "<span class='description'>" + newItem.description + "</span> </li>");
-    $( ".notable").append(itemElement);
+function displayItems(arr) {
+    for (var idx = 0; idx < arr.length; idx++) {
+        var itemElement = $("<li class='notable table-view-cell' id=item'" +"-"+ idx + "'>" + "<span class='description'>" + arr[idx].description + "</span> </li>");
+        $( ".notable").append(itemElement);
+        console.log("displaying item #" + idx + "\n")    };
 }
+loadData ();
