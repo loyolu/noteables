4c05aaeb9f53b4bda61303b28f8f23d802905179
loyolu <loyolu@users.noreply.github.com>
2015-07-29 00:56:42 -0500

    storing in array with unique id

--- function.js
+++ function.js
@@ -7,14 +7,36 @@ function recordText () {
         console.log("enter keyup\n");
         var txt = $("#addbox").val();
         console.log(txt);
+        createData(txt);
    }});
     $(document).on("click", "#addbutton", function(e) {
         console.log("addbutton clicked\n");
         var txt = $("#addbox").val();
         console.log(txt);
+        createData(txt);
     });
     
 }
+
+function createId () {
+    console.log("creating id");
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+    });
+}
+
+function createData (text) {
+    console.log("creating data");
+    var newItem = {
+        "description": text,
+        "id": createId()
+    }
+    console.log("appending new item\n");
+    list.push(newItem);
+    console.log(list, "\n");
+}
+
 
 $(document).ready(function() {
     recordText();
